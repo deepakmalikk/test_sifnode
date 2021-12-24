f93219d8d4c8434b391a50eb526fd819604179d3
Deepak Malik <65271369+deepakmalikk@users.noreply.github.com>
2021-12-24 10:15:24 +0000

    Update msg_test.go

--- msgs_test.go
+++ msgs_test.go
@@ -19,24 +19,61 @@ func TestMsgCreateDistribution(t *testing.T) {
 	output := test.CreatOutputList(2000, "1")
 	authorizedRunner := sdk.AccAddress("addr2_______________")
 	result := types.NewMsgCreateDistribution(distributor, distributionType, output, authorizedRunner.String())
-    err := result.ValidateBasic()
-	assert.Error(t, err)
-	
-	key := result.Route()
-	key1 := types.RouterKey
-	typ := result.Type()
-	typ1 := types.MsgTypeCreateDistribution
-	m := types.MsgCreateDistribution{}
-	byt := m.GetSignBytes()
-	byt1 := sdk.MustSortJSON(types.ModuleCdc.MustMarshalJSON(&m))
-	assert.Equal(t, byt, byt1)
-	assert.Equal(t, typ, typ1)
-	assert.Equal(t, key, key1)
+
 	assert.Equal(t, distributor.String(), result.Distributor)
 	assert.Equal(t, distributionType, result.DistributionType)
 	assert.Equal(t, output, result.Output)
 	assert.Equal(t, authorizedRunner.String(), result.GetAuthorizedRunner())
 
+}
+
+func TestMsgCreateDistribution_Route(t *testing.T) {
+	distributor := sdk.AccAddress("addr1_______________")
+	distributionType := types.DistributionType_DISTRIBUTION_TYPE_AIRDROP
+	output := test.CreatOutputList(2000, "1")
+	authorizedRunner := sdk.AccAddress("addr2_______________")
+	result := types.NewMsgCreateDistribution(distributor, distributionType, output, authorizedRunner.String())
+
+	key := result.Route()
+	key1 := types.RouterKey
+	assert.Equal(t, key, key1)
+}
+
+func TestMsgCreateDistribution_Type(t *testing.T) {
+	distributor := sdk.AccAddress("addr1_______________")
+	distributionType := types.DistributionType_DISTRIBUTION_TYPE_AIRDROP
+	output := test.CreatOutputList(2000, "1")
+	authorizedRunner := sdk.AccAddress("addr2_______________")
+	result := types.NewMsgCreateDistribution(distributor, distributionType, output, authorizedRunner.String())
+
+	typ := result.Type()
+	typ1 := types.MsgTypeCreateDistribution
+	assert.Equal(t, typ, typ1)
+}
+
+func TestMsgCreateDistribution_GetSigners(t *testing.T) {
+	distributor := sdk.AccAddress("addr1_______________")
+	distributionType := types.DistributionType_DISTRIBUTION_TYPE_AIRDROP
+	output := test.CreatOutputList(2000, "1")
+	authorizedRunner := sdk.AccAddress("addr2_______________")
+	m := types.MsgCreateDistribution{
+		Distributor:      string(distributor),
+		DistributionType: distributionType,
+		AuthorizedRunner: string(authorizedRunner),
+		Output:           output,
+	}
+	addr := m.Distributor
+	result := m.GetSigners()
+	addrs, err := sdk.AccAddressFromBech32(addr)
+	t.Log(result)
+	t.Log(addr)
+	t.Log(addrs)
+	t.Log(err)
+
+	// m := types.MsgCreateDistribution{}
+	// byt := m.GetSignBytes()
+	// byt1 := sdk.MustSortJSON(types.ModuleCdc.MustMarshalJSON(&m))
+	// assert.Equal(t, byt, byt1)
 }
 
 func TestMsgCreateDistribution_ValidateBasic(t *testing.T) {
@@ -49,7 +86,6 @@ func TestMsgCreateDistribution_ValidateBasic(t *testing.T) {
 		Output:           test.CreatOutputList(2000, "1"),
 		AuthorizedRunner: authorizedRunner.String(),
 	}
-	t.Log(msg.Distributor)
 	err := msg.ValidateBasic()
 	assert.NoError(t, err)
 }
@@ -127,6 +163,13 @@ func TestMsgCreateDistribution_ValidateBasic_ZeroCoins(t *testing.T) {
 	err := msg.ValidateBasic()
 	assert.Error(t, err)
 }
+func TestMsgCreateDistribution_GetSignBytes(t *testing.T) {
+	m := types.MsgCreateDistribution{}
+	byt := m.GetSignBytes()
+	byt1 := sdk.MustSortJSON(types.ModuleCdc.MustMarshalJSON(&m))
+	assert.Equal(t, byt, byt1)
+
+}
 
 func TestMsgCreateClaim_ValidateBasic_WrongType(t *testing.T) {
 	claimer := sdk.AccAddress("addr1_______________")
@@ -152,21 +195,29 @@ func TestMsgCreateUserClaim(t *testing.T) {
 	userClaimAddress := sdk.AccAddress("addr2_______________")
 	claimType := types.DistributionType_DISTRIBUTION_TYPE_UNSPECIFIED
 	result := types.NewMsgCreateUserClaim(userClaimAddress, claimType)
-	err := result.ValidateBasic()
-	assert.Error(t, err)
+
+	assert.Equal(t, userClaimAddress.String(), result.UserClaimAddress)
+	assert.Equal(t, claimType, result.UserClaimType)
+}
+
+func TestMsgCreateUserClaim_Route(t *testing.T) {
+	userClaimAddress := sdk.AccAddress("addr2_______________")
+	claimType := types.DistributionType_DISTRIBUTION_TYPE_UNSPECIFIED
+	result := types.NewMsgCreateUserClaim(userClaimAddress, claimType)
 
 	key := result.Route()
 	key1 := types.RouterKey
+	assert.Equal(t, key, key1)
+}
+
+func TestMsgCreateUserClaim_Type(t *testing.T) {
+	userClaimAddress := sdk.AccAddress("addr2_______________")
+	claimType := types.DistributionType_DISTRIBUTION_TYPE_UNSPECIFIED
+	result := types.NewMsgCreateUserClaim(userClaimAddress, claimType)
+
 	typ := result.Type()
 	typ1 := types.MsgTypeCreateUserClaim
-	m := types.MsgCreateUserClaim{}
-	byt := m.GetSignBytes()
-	byt1 := sdk.MustSortJSON(types.ModuleCdc.MustMarshalJSON(&m))
-	assert.Equal(t, byt, byt1)
 	assert.Equal(t, typ, typ1)
-	assert.Equal(t, key, key1)
-	assert.Equal(t, userClaimAddress.String(), result.UserClaimAddress)
-	assert.Equal(t, claimType, result.UserClaimType)
 }
 
 func TestMsgRunDistribution(t *testing.T) {
@@ -174,21 +225,30 @@ func TestMsgRunDistribution(t *testing.T) {
 	distributionName := types.AttributeKeyDistributionName
 	distributionType := types.DistributionType_DISTRIBUTION_TYPE_AIRDROP
 	result := types.NewMsgRunDistribution(runner, distributionName, distributionType)
-    err := result.ValidateBasic()
-	assert.Error(t, err)
 
-	key := result.Route()
-	key1 := types.RouterKey
-	typ := result.Type()
-	typ1 := types.MsgTypeRunDistribution
-	m := types.MsgRunDistribution{}
-	byt := m.GetSignBytes()
-	byt1 := sdk.MustSortJSON(types.ModuleCdc.MustMarshalJSON(&m))
-	assert.Equal(t, byt, byt1)
-	assert.Equal(t, typ, typ1)
-	assert.Equal(t, key, key1)
 	assert.Equal(t, runner, result.GetAuthorizedRunner())
 	assert.Equal(t, distributionName, result.DistributionName)
 	assert.Equal(t, distributionType, result.DistributionType)
 
+}
+func TestMsgRunDistribution_Route(t *testing.T) {
+	runner := types.AttributeKeyDistributionRunner
+	distributionName := types.AttributeKeyDistributionName
+	distributionType := types.DistributionType_DISTRIBUTION_TYPE_AIRDROP
+	result := types.NewMsgRunDistribution(runner, distributionName, distributionType)
+
+	key := result.Route()
+	key1 := types.RouterKey
+	assert.Equal(t, key, key1)
+
+}
+func TestMsgRunDistribution_Type(t *testing.T) {
+	runner := types.AttributeKeyDistributionRunner
+	distributionName := types.AttributeKeyDistributionName
+	distributionType := types.DistributionType_DISTRIBUTION_TYPE_AIRDROP
+	result := types.NewMsgRunDistribution(runner, distributionName, distributionType)
+
+	typ := result.Type()
+	typ1 := types.MsgTypeRunDistribution
+	assert.Equal(t, typ, typ1)
 }
